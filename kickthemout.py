a904948af1a1a62934b855f90117f235cd5f746a
David Schütz <xdavidhu@users.noreply.github.com>
2017-01-14 12:13:02 +0100

    Add files via upload

--- kickthemout.py
+++ kickthemout.py
@@ -14,6 +14,11 @@ import urllib2 as urllib
 
 BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)  # Shut up scapy!
+
+if os.geteuid() != 0:
+    print("\n{0}ERROR: KickThemOut must run as root. Try again with sudo/root:\n\t{1}$ sudo python kickthemout.py{2}\n").format(RED, GREEN, END)
+    raise SystemExit
+
 try:
     from scapy.all import *
     import scan, spoof

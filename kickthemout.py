762500d0efd8b26e860dbb829c4f3c145a5c3f85
Eugene Russkikh <zitryss@gmail.com>
2017-07-23 15:59:46 +0200

    Fix ImportError

--- kickthemout.py
+++ kickthemout.py
@@ -16,7 +16,7 @@ try:
 except ImportError:
     import urllib.request as urllib
 
-from builtins import input
+# from builtins import input
 import traceback
 BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
 

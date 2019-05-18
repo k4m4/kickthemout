e636ba5fc92afd2b4b8367ab031e0f0e6cdf76a5
Filippo Ranza <filipporanza@gmail.com>
2019-05-18 22:55:39 +0200

    fixed style issues

--- kickthemout.py
+++ kickthemout.py
@@ -716,8 +716,7 @@ if __name__ == '__main__':
                 '  sudo python3 kickthemout.py --target 192.168.1.10 \n'+
                 '  sudo python3 kickthemout.py -t 192.168.1.5,192.168.1.10 -p 30\n'+
                 '  sudo python3 kickthemout.py -s\n'+
-                '  sudo python3 kickthemout.py -f ip_list.txt\n'+
-                '  sudo python3 kickthemout.py --file ip_list.txt\n'
+                '  sudo python3 kickthemout.py -f IPs.txt\n'+
                 '  sudo python3 kickthemout.py (interactive mode)\n')
 
     parser = optparse.OptionParser(epilog=examples,
@@ -740,14 +739,14 @@ if __name__ == '__main__':
         dest='targets', help='specify target IP address(es) and perform attack')
 
 
-    def target_files(option, opt, value, parser):
+    def targetFiles(option, opt, value, parser):
         with open(value) as file:
-            addr_list = file.readlines()
-            setattr(parser.values, option.dest, addr_list)
+            addrList = file.readlines()
+            setattr(parser.values, option.dest, addrList)
 
     parser.add_option('-f', '--file', action='callback',
-        callback=target_files, type='string',metavar='FILE',
-        dest='targets', help='read target IP address(es), one per line, from given file and perform attack')
+        callback=targetFiles, type='string', metavar='FILE',
+        dest='targets', help='read target IP address(es) from line-separated file and perform attack')
 
     
     (options, argv) = parser.parse_args()
